135ad581169a297afa0c27b027cfdfcffe0832ca
Michelle Low <michellelow@Michelles-MacBook-Pro.local>
2016-05-09 23:32:50 +1000

    first commit

---
+++ pointers.js
@@ -0,0 +1,131 @@
+var pointers = {
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#7f007f",
+        "marker-size": "medium",
+        "marker-symbol": "star"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          144.95198249816895,
+          -37.80707148060925
+        ]
+      },
+      "bbox": [
+        144.95198249816895,
+        -37.80707148060925,
+        144.95198249816895,
+        -37.80707148060925
+      ]
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#7f007f",
+        "marker-size": "medium",
+        "marker-symbol": "star"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          144.96211051940918,
+          -37.809444899409684
+        ]
+      },
+      "bbox": [
+        144.96211051940918,
+        -37.809444899409684,
+        144.96211051940918,
+        -37.809444899409684
+      ]
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#7f007f",
+        "marker-size": "medium",
+        "marker-symbol": "star"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          144.9514675140381,
+          -37.81615787094836
+        ]
+      },
+      "bbox": [
+        144.9514675140381,
+        -37.81615787094836,
+        144.9514675140381,
+        -37.81615787094836
+      ]
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#7f007f",
+        "marker-size": "medium",
+        "marker-symbol": "star"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          144.9609088897705,
+          -37.81737834565081
+        ]
+      },
+      "bbox": [
+        144.9609088897705,
+        -37.81737834565081,
+        144.9609088897705,
+        -37.81737834565081
+      ]
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#7f007f",
+        "marker-size": "medium",
+        "marker-symbol": "star"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          144.97206687927246,
+          -37.8006289593366
+        ]
+      },
+      "bbox": [
+        144.97206687927246,
+        -37.8006289593366,
+        144.97206687927246,
+        -37.8006289593366
+      ]
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#7f007f",
+        "marker-size": "medium",
+        "marker-symbol": "star"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          144.95507240295407,
+          -37.7983230783235
+        ]
+      },
+      "bbox": [
+        144.95507240295407,
+        -37.7983230783235,
+        144.95507240295407,
+        -37.7983230783235
+      ]
+    }
+  ]
+};
